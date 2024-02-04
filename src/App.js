3e70b941c9e92c2d9cbbbd04adf2f4cae4236e5c
Eric Yon <ericyon24@gmail.com>
2024-02-04 16:28:32 -0600

    design changes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
 import "./App.css";
-import { useState } from "react";
-import Logo from "./images/logo.svg";
-import Close from "./images/icon-close-menu.svg";
-import Open from "./images/icon-menu.svg";
-import ArrowDown from "./images/icon-arrow-down.svg";
-import ArrowUp from "./images/icon-arrow-up.svg";
+import Nav from "./components/Nav";
 
 function App() {
-  const [menu, setMenu] = useState(false);
-
-  const handleMenu = () => {
-    setMenu(!menu);
-  };
-
   return (
     <div className="App">
-      <header className="nav">
-        <img className="logo-img" src={Logo} alt="" />
-        <div className="menu-icon-container">
-          <img src={menu ? Close : Open} alt="" onClick={handleMenu} />
-        </div>
-        <div className="desktop-menu">
-          <div className="links">
-            <span className="link">
-              Features
-              <img className="arrow" src={ArrowDown} alt="" />
-            </span>
-            <span className="link">
-              Company
-              <img className="arrow" src={ArrowDown} alt="" />
-            </span>
-            <span className="link">Careers</span>
-            <span className="link">About</span>
-          </div>
-          <div className="logins">
-            <span className="link">Login</span>
-            <button className="register">Register</button>
-          </div>
-        </div>
-      </header>
+      <Nav />
       <div></div>
     </div>
   );
--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -1,5 +1,18 @@
 const NavMenu = () => {
-  return <div></div>;
+  return (
+    <div className="menu-container">
+      <div className="menu-links">
+        <span className="link">Features</span>
+        <span className="link">Company</span>
+        <span className="link">Careers</span>
+        <span className="link">About</span>
+      </div>
+      <div className="menu-login">
+        <span className="link">Login</span>
+        <button className="register">Register</button>
+      </div>
+    </div>
+  );
 };
 
 export default NavMenu;
